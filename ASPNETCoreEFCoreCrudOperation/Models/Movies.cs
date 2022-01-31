925481aaab2e727f2750b51f2a824ecc438ce030
James Purnadi <jamesp.4907@gmail.com>
2022-01-31 08:51:50 -0600

    dyd1

---
+++ ASPNETCoreEFCoreCrudOperation/Models/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreEFCoreCrudOperation.Models
+{
+    public class Movies
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Please enter Movie Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter Producer Name")]
+        public string Producer { get; set; }
+        public string Company { get; set; }
+        public double RatingOutof10 { get; set; }
+
+    }
+}
---
+++ ASPNETCoreEFCoreCrudOperation/Models/MoviesContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreEFCoreCrudOperation.Models
+{
+    //DbContext is using Microsoft Entity Framework Core via the packages installed
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext()
+        {
+
+        }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options)
+        {
+
+        }
+
+        //DbSet correspond to a particular table which will be created via Entity Framework
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
